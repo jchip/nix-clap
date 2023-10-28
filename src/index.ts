6b7de228fbdb5678fe9f659cb1b1498c327593bb
Joel Chen <joel123@gmail.com>
2023-10-28 15:09:45 -0700

    5

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { NixClap, NixClapConfig } from "./nix-clap";
+export { CommandSpec, CommandExecFunc, Command } from "./command";
+export { OptionSpec, OptionsData, Options } from "./options";
+export { Commands, CommandsData } from "./commands";
